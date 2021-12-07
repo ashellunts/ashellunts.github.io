a83cdc1ca37707768e4bf2a46d72e65ef276f73c
Artur Shellunts <shellunts.artur@gmail.com>
2021-12-07 17:18:50 +0100

    Add back visitors count

--- blog/count_visitors.js
+++ blog/count_visitors.js
@@ -1,8 +1,22 @@
-let visitors_add_url = "https://ashelluntsgithubio.ashellunts.repl.co/visitors/add";
-let visitors_count_url = "https://ashelluntsgithubio.ashellunts.repl.co/visitors/count";
+let visitors_add_url = "https://8bumfwcy4k.execute-api.eu-central-1.amazonaws.com/prod";
+let visitors_count_url = "https://8bumfwcy4k.execute-api.eu-central-1.amazonaws.com/prod";
 
 function add_visitor() {
-    fetch(visitors_add_url, { method: "POST"}).then(handleResponse)
+	url = window.location.pathname
+	
+	document.getElementById("today_page_visits").innerHTML = url
+	
+	return
+	
+	fetch(
+		visitors_add_url,{
+			method: "POST",
+			//headers: {
+				//'Accept': 'application/json',
+				//'Content-Type': 'application/json'
+			//},
+			body: JSON.stringify({page: 'blog1'})
+		}).then(handleResponse)
 }
 
 async function handleResponse(response) {
